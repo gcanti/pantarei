101cb0ddb2df9ff2782888b728b5d9aaf290d435
gcanti <giulio.canti@gmail.com>
2016-06-23 16:53:38 +0200

    Middleware

--- redux/3.5.2+.js
+++ redux/3.5.2+.js
@@ -2,16 +2,22 @@ declare module 'redux' {
 
   declare type Dispatch<Action> = (action: Action) => any;
 
+  declare type Unsubscriber = () => void;
+
   declare type Store<State, Action> = {
     dispatch: Dispatch<Action>;
     getState(): State;
-    subscribe(listener: () => any): () => void;
+    subscribe(listener: () => any): Unsubscriber;
   };
 
-  declare type Reducer<State, Action, ReduxAction> = (state: State, action: Action | ReduxAction) => State;
+  declare type Reducer<State, Action, ThirdPartyAction> = (state: State, action: Action | ThirdPartyAction) => State;
+
+  declare type Middleware<State, Action> = (store: Store<State, Action>) => (next: Dispatch<Action>) => (action: Action) => State;
 
   declare var exports: {
-    createStore<State, Action, ReduxAction>(reducer: Reducer<State, Action, ReduxAction>, preloadedState?: State): Store<State, Action>;
-    combineReducers<State, Action, ReduxAction>(reducers: Object): Reducer<State, Action, ReduxAction>;
+    createStore<State, Action, ThirdPartyAction>(reducer: Reducer<State, Action, ThirdPartyAction>, preloadedState?: State): Store<State, Action>;
+    combineReducers<State, Action, ThirdPartyAction>(reducers: Object): Reducer<State, Action, ThirdPartyAction>;
+    applyMiddleware<State, Action>(...middlewares: Array<Middleware<State, Action>>): void;
+    compose(...fn: Array<Function>): Function;
   }
 }
